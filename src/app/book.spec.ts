16ead95c475e7f46039a8855f4f43711b5e3e7b8
Rodrigo Vilar <rvilar@gmail.com>
2017-04-01 19:09:00 -0300

    Adding Book DTO class

---
+++ src/app/book.spec.ts
@@ -0,0 +1,12 @@
+import {Book} from './book';
+
+describe('Book', () => {
+  it('should create an instance', () => {
+    expect(new Book(null,null)).toBeTruthy();
+  });
+  it('should accept values in the constructor', () => {
+    let book = new Book('ng-book 2', 'Nate Murray');
+    expect(book.name).toEqual('ng-book 2');
+    expect(book.author).toEqual('Nate Murray');
+  });
+});
